1c602aad5aa830b601b361fcdadb4adbadac8df8
shikikiriti <43218573+shikikiriti@users.noreply.github.com>
2018-09-13 00:25:08 +0500

    some changes

--- admin/ban-bot.js
+++ admin/ban-bot.js
@@ -2,7 +2,14 @@ module.exports = async ({ message, kickChatMember, deleteMessage }) => {
   const blacklistes = message.new_chat_members.filter((member) => (
     member.is_bot ||
     member.first_name.startsWith('╋VX,QQ（同号）') ||
-    member.first_name.startsWith('╋VX(QQ)')
+    member.first_name.startsWith('╋VX(QQ)') || 
+    message.startsWith('НЕ БЛАГОДАРИ') || 
+    message.startsWith('НАЧНИ ТРЕЙДИТЬ') || 
+    message.startsWith('НАЧНИ ТОРГОВАТЬ') || 
+    message.startsWith('ТЫ БУДЕШЬ В ШОКЕ') || 
+    message.startsWith('Нашёл инсайд') || 
+    message.startsWith('-') || 
+    message.startsWith('Каспер сосатб')   
   ))
 
   if (blacklistes.length > 0) {
